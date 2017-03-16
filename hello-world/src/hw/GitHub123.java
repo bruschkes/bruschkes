8c04495c05e59c5b1de35be8a52b58f0b35066f2
bruschkes <S.Bruschke@gmx.de>
2017-03-16 12:49:47 +0100

    # WARNING: head commit changed in the meantime
    newest_version

---
+++ hello-world/src/hw/GitHub123.java
@@ -0,0 +1,14 @@
+package hw;
+
+public class GitHub123 {
+
+	public static void main(String[] args) {
+
+
+		System.out.println("GitHub Test erfolgreich");
+		//		kommentar
+		
+		System.out.println("test");
+	}
+
+}
